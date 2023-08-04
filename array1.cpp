b9730745cb4622b1ba83c274d0d1768d35b1b42e
Atul Kumar Tiwari <atulkumartiwari@Atuls-MacBook-Air.local>
2023-08-04 11:52:07 +0530

    array

---
+++ array1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[10], n, i;
+    cout<<"Enter the range of elements: ";
+    cin>>n;
+    cout<<"Enter the elements: ";
+    for(i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+    cout<<"Entered elements are: ";
+    for(i=0; i<n; i++)
+    {
+        cout<<a[i]<<"\t";
+    }
+    return 0;
+}
