324c00b728213fba42929f212e536a5eeedd7cd1
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2023-08-25 11:37:21 +0530

    double linked list

---
+++ linkedList_double.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+struct link
+{
+    int data;
+    link *next;
+    link *prev;
+};
+link *start=NULL;
+link *current;
+link *node;
+
+void create()
+{
+    int n;
+    cout<<"Enter the number of nodes: ";
+    cin>>n;
+    for(int i=0; i<n; i++)
+    {
+        cout<<"Enter the data: ";
+        node=(link *)malloc(sizeof(link));
+        cin>>node->data;
+        node->next=NULL;
+        node->prev=NULL;
+        if(start==NULL)
+        {
+            start=node;
+            current=node;
+        }
+        else
+        {
+            current->next = node;
+            node->prev = current;
+            current = node;
+        }
+    }
+}
+void display()
+{
+    link *ptr = start;
+    while(ptr!=NULL)
+    {
+        cout<<ptr->prev<<"\t";
+        cout<<ptr->data<<"  ";
+        cout<<ptr<<"\t"<<endl;
+        ptr=ptr->next;
+    }
+}
+int main()
+{
+    create();
+    display();
+    return 0;
+}
