7660ff2586788b5a861929d214e29a27a428be87
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2023-09-01 11:04:25 +0530

    merging two linked list

--- linkedListMerging.cpp
+++ linkedListMerging.cpp
@@ -38,72 +38,71 @@ void create(struct link *&start)
 }
 void display(struct link *&start)
 {
-struct link *ptr=start;
-while(ptr!=NULL)
-{
-cout<<ptr->data << " ";
-ptr=ptr->next;
-}
-cout << endl;
+    struct link *ptr=start;
+    while(ptr!=NULL)
+    {
+        cout<<ptr->data << " ";
+        ptr=ptr->next;
+    }
+    cout << endl;
 }
 
 void merge(struct link *&s1, struct link *&s2)
 {
-struct link *node;
-while(s1 != NULL && s2 != NULL)
-{
-node = (struct link *)malloc(sizeof(struct link));
-if(s1->data < s2->data)
-{
-node -> data = s1 -> data;
-s1 = s1 -> next;
-}
-else
-{
-node -> data = s2 -> data;
-s2 = s2 -> next;
-}
-node -> next = NULL;
-if(mergeStart == NULL)
-{
-mergeStart = node;
-mergeCurrent = node;
-}
-else
-{
-mergeCurrent -> next = node;
-mergeCurrent = node;
-}
-}
-while(s1 != NULL)
-{
-node = (struct link *)malloc(sizeof(struct link));
-node -> data = s1 ->data;
-mergeCurrent -> next = node;
-mergeCurrent = node;
-s1 = s1 -> next;
-}
-while(s2 != NULL)
-{
-node = (struct link *)malloc(sizeof(struct link));
-node -> data = s2 ->data;
-mergeCurrent -> next = node;
-mergeCurrent = node;
-s2 = s2 -> next;
-}
+    struct link *node;
+    while(s1 != NULL && s2 != NULL)
+    {
+        node = (struct link *)malloc(sizeof(struct link));
+        if(s1->data < s2->data)
+        {
+            node -> data = s1 -> data;
+            s1 = s1 -> next;
+        }
+        else
+        {
+            node -> data = s2 -> data;
+            s2 = s2 -> next;
+        }
+        node -> next = NULL;
+        if(mergeStart == NULL)
+        {
+            mergeStart = node;
+            mergeCurrent = node;
+        }
+        else
+        {
+            mergeCurrent -> next = node;
+            mergeCurrent = node;
+        }
+    }
+    while(s1 != NULL)
+    {
+        node = (struct link *)malloc(sizeof(struct link));
+        node -> data = s1 ->data;
+        mergeCurrent -> next = node;
+        mergeCurrent = node;
+        s1 = s1 -> next;
+    }
+    while(s2 != NULL)
+    {
+        node = (struct link *)malloc(sizeof(struct link));
+        node -> data = s2 ->data;
+        mergeCurrent -> next = node;
+        mergeCurrent = node;
+        s2 = s2 -> next;
+    }
 }
 int main()
 {
-
-cout<<"Enter first LinkedList"<<endl;
-create(start1);
-cout<<"Enter second LinkedList"<<endl;
-create(start2);
-cout<<"Elements of linked list one: "<<endl;
-display(start1);
-cout<<"Elements of linked list two: "<<endl;
-display(start2);
-merge(start1, start2);
-display(mergeStart);
-return 0;
+    cout<<"Enter first LinkedList"<<endl;
+    create(start1);
+    cout<<"Enter second LinkedList"<<endl;
+    create(start2);
+    cout<<"Elements of linked list one: "<<endl;
+    display(start1);
+    cout<<"Elements of linked list two: "<<endl;
+    display(start2);
+    merge(start1, start2);
+    display(mergeStart);
+    return 0;
 }
