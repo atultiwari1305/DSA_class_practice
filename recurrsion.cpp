649736cd53c91c5a118d8aace96854d51782e33a
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2023-10-04 11:19:36 +0530

    recurrsion

---
+++ recurrsion.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void fun(int n)
+{
+    if(n>0)
+    {
+        fun(n-1);
+        cout<<n<<" ";
+    }
+}
+int main()
+{
+    int n=5;
+    fun(n);
+    return 0;
+}
