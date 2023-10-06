6f35948a63e5dae491d1cd225992deba116c0661
Atul Kumar Tiwari <atulrajtiwari098@gmail.com>
2023-10-06 17:47:47 +0530

    recurrsion

--- recurrsion.cpp
+++ recurrsion.cpp
@@ -2,15 +2,17 @@
 using namespace std;
 void fun(int n)
 {
-    if(n>0)
+    if(n==0)
     {
-        fun(n-1);
-        cout<<n<<" ";
+        return;
     }
+    fun(n-1);
+    cout<<n<<" ";
+    fun(n-1);
 }
 int main()
 {
-    int n=5;
+    int n=4;
     fun(n);
     return 0;
 }
